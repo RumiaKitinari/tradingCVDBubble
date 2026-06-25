39da0631e748f0528d8647f88a379dcfff63644d
Jason Zheng <jasonzheng7@gmail.com>
2026-06-25 13:14:10 -0400

    chore: init

--- quickstart/sample_mflix/apps.py
+++ quickstart/sample_mflix/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CvdviewConfig(AppConfig):
+class SampleMflixConfig(AppConfig):
     default_auto_field = 'django_mongodb_backend.fields.ObjectIdAutoField'
-    name = 'cvdView'
+    name = 'sample_mflix'
